03e2900d5a908b770fc2558d0e1e5be69317b73e
chaemin <w2114@e-mirim.hs.kr>
2023-06-07 13:47:39 +0900

    리스트

--- app/src/main/java/com/example/todopizza_app/todo_target.java
+++ app/src/main/java/com/example/todopizza_app/todo_target.java
@@ -15,21 +15,18 @@ import androidx.annotation.Nullable;
 
 public class todo_target extends Activity {
     Button ListAddButton;
-    LinearLayout listAdd;
+    LinearLayout Addlist;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.todo_target);
         ListAddButton = findViewById(R.id.ListAddButton);
-        listAdd = findViewById(R.id.listAdd);
 
         ListAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LayoutInflater inflater = (LayoutInflater)
-                        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                inflater.inflate(R.layout.todo_list, listAdd, true);
+                Addlist.setVisibility(View.VISIBLE);
             }
         });
     }
