537a74a19fe42e6d306fbccfcd01c789d8412fda
chaemin <w2114@e-mirim.hs.kr>
2023-06-21 01:22:22 +0900

    최종

--- app/src/main/java/com/example/todopizza_app/MainActivity.java
+++ app/src/main/java/com/example/todopizza_app/MainActivity.java
@@ -140,6 +140,7 @@ public class MainActivity extends AppCompatActivity {
                                 revalidatePizzaToppings();
                             }
                         });
+                       revalidatePizzaToppings();
 //                        if(targetnum == targetnum2){ //targetnum은 목표 추가 버튼을 클릭하면 증가하는 값. 이거랑 __비교해야해
 //                            //__ 이 값은 지금 누른 버튼의 값이여야해
 //                            //지금 내 생각은 지정한 아이디값이랑 뭘 비교해야해 근데 그 뭘 모르겟어
@@ -174,7 +175,6 @@ public class MainActivity extends AppCompatActivity {
 
         today = findViewById(R.id.today);
         targetbtn = findViewById(R.id.targetbtn);
-        CalMove = findViewById(R.id.CalMove);
         TargetAdd = findViewById(R.id.TargetAdd);
 
 
@@ -184,21 +184,26 @@ public class MainActivity extends AppCompatActivity {
         String time = mFormat.format(date);
         today.setText(time);
 
-        CalMove.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), calender.class);
-                startActivity(intent);
-                finish(); //캘린더로 넘어가는 화면
-            }
-        });
     }
 
+    /*
+    * 이제 문제는 if (box.isChecked() == true) checkCount++; 여기서 작동이 이상함
+    * 체크박스를 해제하면
+    * 리스트를 전체로 보는듯. 하나하나씩 비교해야하는데
+    * 그래서 리스트 하나만 체크해도 true로 나와서 하나만 체크해도 토핑이 올라가는것..
+    * <-- 목표 리스트는 잇으니까.. 리스트를 또 따로 배열에  저장해줘야하나
+    *
+    * 그리고 목표 추가를 누르면 쟤네가 초기값으로 변하고 원래 잇던 목표 수정을 누르면 누른 값을 가져와줘야지
+    * 그래서 목표 아이디를 따로 줘서 배열에 저장한 거 잖아 아니 무슨 코드가 왜이리 어려워
+    *
+    * 토핑의 이름이 바뀌면 원래 올라가있던 토핑을 보이지 않게 설정해줘야해
+    * del을 누르면 totalCount -1씩 해줘야지.
+    * */
     public void revalidatePizzaToppings() {
-        String name = spinnerNames[selected_fruit_idx];
-        Log.d("mytag",""+name);
+        String name = spinnerNames[selected_fruit_idx]; //토핑의 이름 가져오기
+        Log.d("mytag",""+name); //가져온 토핑 이름 출력
        // Log.d("mytag", "revalidatePizzaToppings");
-        Log.d("mytag1",selected_fruit_idx+"");
+        Log.d("mytag",selected_fruit_idx+""); //토핑의 배열 위치 숫자
         LinearLayout listContainer = findViewById(R.id.TargetAdd);
         // Log.d("mytag", listContainer.getChildCount() + "");
         for (int i = 0; i < listContainer.getChildCount(); i++) {
@@ -210,46 +215,49 @@ public class MainActivity extends AppCompatActivity {
             // Log.d("mytag", spinner.getSelectedItem().toString());
             LinearLayout list = (LinearLayout) targetList.findViewById(R.id.list);
             int totalCount = list.getChildCount();
-            Log.d("mytag2", list.getChildCount() + "");
+            Log.d("mytag", list.getChildCount() + ""); //하나의 목표에 들어있는 리스트 개수
+
             int checkCount = 0;
             for (int j = 0; j < list.getChildCount(); j++) {
                 CheckBox box = list.findViewById(R.id.listBox); //체크박스
-                Log.d("mytag3", box.isChecked() + "");
-                if (box.isChecked() == true) checkCount++; //체크박스 체크 여부 개수랑 totalCount비교
+                Log.d("mytag", box.isChecked() + ""); //목표 안에 들어있는 리스트 true false여부 출력
+               //for(int a=0; a<=totalCount; a++) {
+                    if (box.isChecked() == true) {
+                        checkCount++;//체크박스 체크 여부 개수랑 totalCount비교
+                    }
+               //}
             }
-            Log.d("chenum value",""+chenum);
-            Log.d("checkCount value",""+checkCount);
+            Log.d("mytag",""+totalCount);
+            Log.d("mytag",""+checkCount);
 
-            if (chenum == checkCount) {
-                String spinnerName = spinnerNames[selected_fruit_idx];
-                if (spinnerName == "초록피망") {
+            if (totalCount != 0 && checkCount != 0 && totalCount == checkCount) {
+                if (name == "초록피망") {
                     gpimentoToppings.setVisibility(View.VISIBLE);
-                } else if (spinnerName == "햄") {
+                } else if (name == "햄") {
                     hamToppings.setVisibility(View.VISIBLE);
-                } else if (spinnerName == "버섯") {
+                } else if (name == "버섯") {
                     mushroomToppings.setVisibility(View.VISIBLE);
-                } else if (spinnerName == "올리브") {
+                } else if (name == "올리브") {
                     oliveToppings.setVisibility(View.VISIBLE);
-                } else if (spinnerName == "양파") {
+                } else if (name == "양파") {
                     onionToppings.setVisibility(View.VISIBLE);
-                } else if (spinnerName == "빨강피망") {
+                } else if (name == "빨강피망") {
                     rpimentoToppings.setVisibility(View.VISIBLE);
                 } else {
                     tomatoToppings.setVisibility(View.VISIBLE);
                 }
             }else if(totalCount != checkCount){
-                String spinnerName = spinnerNames[selected_fruit_idx];
-                if (spinnerName == "초록피망") {
+                if (name == "초록피망") {
                     gpimentoToppings.setVisibility(View.INVISIBLE);
-                } else if (spinnerName == "햄") {
+                } else if (name == "햄") {
                     hamToppings.setVisibility(View.INVISIBLE);
-                } else if (spinnerName == "버섯") {
+                } else if (name == "버섯") {
                     mushroomToppings.setVisibility(View.INVISIBLE);
-                } else if (spinnerName == "올리브") {
+                } else if (name == "올리브") {
                     oliveToppings.setVisibility(View.INVISIBLE);
-                } else if (spinnerName == "양파") {
+                } else if (name == "양파") {
                     onionToppings.setVisibility(View.INVISIBLE);
-                } else if (spinnerName == "빨강피망") {
+                } else if (name == "빨강피망") {
                     rpimentoToppings.setVisibility(View.INVISIBLE);
                 } else {
                     tomatoToppings.setVisibility(View.INVISIBLE);
